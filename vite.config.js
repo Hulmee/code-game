73f75e5e6d1f8c3f62d6f759692aa6c1a2455505
Ellery <elleryh@insightsystems.com.au>
2024-02-05 16:53:40 +1100

    add PWA and change #app sizing to use dvh & dvw

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,19 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: 'lock.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
